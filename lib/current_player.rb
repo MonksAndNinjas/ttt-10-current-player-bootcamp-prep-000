48981c83e5fb7bc33116a1bde5d68469000570ab
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-04-11 18:17:25 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,7 +3,7 @@ def turn_count(board)
   count = 0
   until count === 9
     board.each do |position|
-      if position === "X" || position === "O"
+      if (position === "X") || (position === "O")
       count += 1
       else
       count += 0
